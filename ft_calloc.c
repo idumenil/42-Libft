6c1d154f7bcae00fea8b42356f882a7c4c183c4d
Irène Duménil <126900447+idumenil@users.noreply.github.com>
2023-03-13 09:45:46 +0100

    Update ft_calloc pour le cas nul

--- ft_calloc.c
+++ ft_calloc.c
@@ -17,7 +17,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void	*ptr;
 
 	if (nmemb == 0 || size == 0)
-		malloc(0);
+		return (malloc(0));
 	if ((nmemb * size) / size != nmemb)
 		return (NULL);
 	ptr = (void *)malloc(nmemb * size);
